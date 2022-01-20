1e134df2244cbe8f2a08b8f5b7e4a0e56455bd71
ztc <1608864756@qq.com>
2022-01-20 15:47:52 +0800

    style: example project homepage px changed to rpx

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -23,21 +23,21 @@
 		box-sizing: border-box;
 		width: 100%;
 		min-height: 100vh;
-		padding: 46px 20px 20px;
+		padding: 92rpx 40rpx 40rpx;
 		background: linear-gradient(131.16deg, rgba(240, 242, 247, 1) 0%, rgba(255, 250, 250, 1) 100%, rgba(216, 220, 230, 1) 100%);
 	}
 
 	.demo-home__title,
 	.demo-home__desc {
-		padding-left: 16px;
+		padding-left: 32rpx;
 		font-weight: normal;
 		line-height: 1;
 		user-select: none;
 	}
 
 	.demo-home__title {
-		margin: 0 0 16px;
-		font-size: 32px;
+		margin: 0 0 32rpx;
+		font-size: 64rpx;
 	}
 
 	.demo-home__image,
@@ -47,22 +47,22 @@
 	}
 
 	.demo-home__image {
-		width: 32px;
-		height: 32px;
+		width: 64rpx;
+		height: 64rpx;
 	}
 
 	.demo-home__text {
-		margin-left: 16px;
+		margin-left: 32rpx;
 		font-weight: 500;
 	}
 
 	.demo-home__title .demo-home--small {
-		font-size: 24px;
+		font-size: 48rpx;
 	}
 
 	.demo-home__desc {
-		margin: 0 0 40px;
+		margin: 0 0 80rpx;
 		color: rgba(69, 90, 100, 0.6);
-		font-size: 14px;
+		font-size: 28rpx;
 	}
 </style>
